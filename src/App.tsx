c558a8d4fb57bb8fb48b50db7ad53ede6e9f1fea
Tony Noda PC Windows <noda.thonny@gmail.com>
2022-06-23 00:03:14 -0300

    Aula 03 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { ApolloProvider } from "@apollo/client";
 import { Event } from "./pages/Event";
+import { Router } from "./Router";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <Event />;
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
