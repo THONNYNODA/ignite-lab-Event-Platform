734ead0bfdcbdfb268f6c24b9f475f5f3a6473a2
Tony Noda PC Windows <noda.thonny@gmail.com>
2022-06-25 07:28:19 -0300

    Aula04 error no 400

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client  = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4omp3e60iu401xkag3f5fdl/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers:{
+        'Authorization':`Bearer ${import.meta.env.VITE_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 });
